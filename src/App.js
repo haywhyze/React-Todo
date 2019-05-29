02fef747641b5a4b49ef8a59103400d181debb0d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 13:11:29 +0100

    initialize state object on App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ class App extends React.Component {
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoList: intitialTodo,
+      todoName: '',
+    }
+  }
+
   render() {
     return (
       <div>
