21f9c334be04dff0468f3a1c7b37411b654defdd
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 12:59:36 +0100

    Initial TodoList component Markup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
-import Todo from './components/TodoComponents/Todo';
+import TodoList from './components/TodoComponents/TodoList';
+
+const intitialTodo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <li key='2'><Todo todoName='Complete MVP' /></li>
-        <li key='1'><Todo todoName='Attempt Stretch' /></li>
+        <TodoList todoList={intitialTodo} />
         <TodoForm />
       </div>
     );
