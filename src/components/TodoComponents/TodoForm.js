79ecf77a7f6b31d882004d1f7a9f9eb8e732360a
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-29 13:12:04 +0100

    Added props needed by TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Todo.css';
 
-export default () => (
+export default ({ todoName, handleChange, handleAdd, handleClear }) => (
   <form>
     <input
       type='text'
+      onChange={handleChange}
+      value={todoName}
     />
     <input
       type='submit'
       value='Add Todo'
+      onClick={handleAdd}
     />
-    <button>Clear Completed</button>
+    <button onClick={handleClear}>Clear Completed</button>
   </form>
 );
